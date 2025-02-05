c2eebd18d678d724f777b035ac37e90015ade6ea
omarelhamshary <omar.elhamshary94@gmail.com>
2025-02-05 22:25:28 +0400

    Implemented main method and quick test for adding a new task

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
 import java.awt.BorderLayout;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to TaskPlanner");
-        System.out.println("How can I help you?");
+        TaskManager newTask = new TaskManager();
+        boolean applicationIsRunning = true;
+        newTask.createFile();
+
+        while (applicationIsRunning) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please choose an option:");
+            System.out.println("1. Create a task");
+            System.out.println("2. Update list of tasks");
+            String option = scanner.nextLine();
+
+            switch (option) {
+                case "1":{
+                    System.out.println("Please enter the name of the task you would like to create:");
+                    String taskDescription = scanner.nextLine();
+                    newTask.AddTask(taskDescription);
+                    break;
+                }
+                case "2":{
+                    System.out.println("Please enter the name of the task you would like to update:");
+                    break;
+                }
+            }
+        }
+
+
     }
 }
